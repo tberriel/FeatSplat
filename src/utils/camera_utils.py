df928587b432913f83f398ab8eff6eb24f3de9c3
tberriel <57666961+tberriel@users.noreply.github.com>
2024-02-16 16:50:14 +0100

    commented leaked lines from semantic branch

--- src/utils/camera_utils.py
+++ src/utils/camera_utils.py
@@ -39,15 +39,19 @@ def loadCam(args, id, cam_info, resolution_scale):
         resolution = (int(orig_w / scale), int(orig_h / scale))
 
     resized_image_rgb = PILtoTorch(cam_info.image, resolution)
-    resized_semantic = SemanticPILtoTorch(cam_info.semantic_image, resolution)
+    #resized_semantic = SemanticPILtoTorch(cam_info.semantic_image, resolution)
 
     gt_image = resized_image_rgb[:3, ...]
-    gt_semantic = resized_semantic[:1, ...]
+    #gt_semantic = resized_semantic[:1, ...]
     loaded_mask = None
 
     if resized_image_rgb.shape[1] == 4:
         loaded_mask = resized_image_rgb[3:4, ...]
 
+    return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
+                  FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
+                  image=gt_image, gt_alpha_mask=loaded_mask,
+                  image_name=cam_info.image_name, uid=id, data_device=args.data_device)
     return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
                   FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
                   image=gt_image, semantic_image=gt_semantic, gt_alpha_mask=loaded_mask,
