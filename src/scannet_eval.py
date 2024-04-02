25efdbe2912fca94b53c3ff1c366b520ea56bccb
tberriel <57666961+tberriel@users.noreply.github.com>
2024-04-02 16:20:44 +0200

    Fixed metrics computation

--- src/scannet_eval.py
+++ src/scannet_eval.py
@@ -11,8 +11,10 @@
 
 import os
 from argparse import ArgumentParser
-scannetpp_scenes =  ['a5114ca13d','108ec0b806','bb87c292ad', 'ebc200e928', 'a08d9a2476','5942004064']#['0a5c013435', 'f07340dfea',  '7bc286c1b6', 'd2f44bf242',  '85251de7d1', '0e75f3c4d9', '98fe276aa8', '7e7cd69a59', 'f3685d06a9', '21d970d8de', '8b5caf3398', 'ada5304e41', '4c5c60fa76']#'a5114ca13d','108ec0b806','bb87c292ad', 'ebc200e928', 'a08d9a2476','5942004064',
-
+from metrics import evaluate
+import json
+scannetpp_scenes =  ['0a5c013435', 'f07340dfea',  '7bc286c1b6', 'd2f44bf242',  '85251de7d1', '0e75f3c4d9', '98fe276aa8', '7e7cd69a59', 'f3685d06a9', '21d970d8de', '8b5caf3398', 'ada5304e41', '4c5c60fa76']#'a5114ca13d','108ec0b806','bb87c292ad', 'ebc200e928', 'a08d9a2476','5942004064',
+iterations = 4
 parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument("--skip_training", action="store_true")
 parser.add_argument("--skip_rendering", action="store_true")
@@ -34,18 +36,42 @@ if not args.skip_rendering:
  
 
     common_args = " --quiet --eval --skip_train"
-    for i in range (4):
+    for i in range (iterations):
         for n_classes, lambda_sem in [[0,0.0],[64,0.001]]:
             for scene, source in zip(all_scenes, all_sources):
-                #os.system("python src/render.py --iteration 7000 -s " + source + " -m " + args.output_path + "/" + f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i} " + common_args)
-                os.system("python src/render.py --iteration 30000 -s " + source + " -m " + args.output_path + "/" + f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i} " + common_args)
+                #os.system("python src/render.py --iteration 7000 -s " + source + " -m " + args.output_path + "/" + f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i}" + common_args)
+                os.system("python src/render.py --iteration 30000 -s " + source + " -m " + args.output_path + "/" + f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i}" + common_args)
 
 if not args.skip_metrics:
+    scenes_list = []
     scenes_string = ""
-    for i in range(4):
+    for i in range(iterations):
         for n_classes, lambda_sem in [[0,0.0],[64,0.001]]:
             for scene in all_scenes:
+                scenes_list.append(args.output_path + "/" + f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i}")
                 scenes_string += "\"" + args.output_path + "/" + f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i} "+ "\" "
+    evaluate(scenes_list)
 
-    os.system("python src/metrics.py -m " + scenes_string)
+for n_classes, lambda_sem in [[0,0.0],[64,0.001]]:
+    results = []
+    for i in range(iterations):
+        for scene in all_scenes:
+
+            result_file = os.path.join(args.output_path, f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i}", "results.json")
+            with open(result_file, "r") as f:
+                data = json.load(f)
+                ssim = data["SSIM"]
+                lpips = data["LPIPS"]
+                psnr = data["PSNR"]
+                results.append((ssim, lpips, psnr))
+
+    # Compute averages
+    avg_ssim = sum([result[0] for result in results]) / len(results)/iterations
+    avg_lpips = sum([result[1] for result in results]) / len(results)/iterations
+    avg_psnr = sum([result[2] for result in results]) / len(results)/iterations
+    print(f"DS with {n_classes} ")
+    print("Average SSIM:", avg_ssim)
+    print("Average LPIPS:", avg_lpips)
+    print("Average PSNR:", avg_psnr)
+#os.system("python src/metrics.py -m " + scenes_string)
 
