7965390eee59fa2958250687a270a1741be3b3c2
tberriel <57666961+tberriel@users.noreply.github.com>
2024-04-03 11:46:13 +0200

    Added average computation to  scannet_eval

--- src/scannet_eval.py
+++ src/scannet_eval.py
@@ -13,7 +13,7 @@ import os
 from argparse import ArgumentParser
 from metrics import evaluate
 import json
-scannetpp_scenes =  ['0a5c013435', 'f07340dfea',  '7bc286c1b6', 'd2f44bf242',  '85251de7d1', '0e75f3c4d9', '98fe276aa8', '7e7cd69a59', 'f3685d06a9', '21d970d8de', '8b5caf3398', 'ada5304e41', '4c5c60fa76']#'a5114ca13d','108ec0b806','bb87c292ad', 'ebc200e928', 'a08d9a2476','5942004064',
+scannetpp_scenes = ['a5114ca13d','108ec0b806','bb87c292ad', 'ebc200e928', 'a08d9a2476','5942004064',]#  ['0a5c013435', 'f07340dfea',  '7bc286c1b6', 'd2f44bf242',  '85251de7d1', '0e75f3c4d9', '98fe276aa8', '7e7cd69a59', 'f3685d06a9', '21d970d8de', '8b5caf3398', 'ada5304e41', '4c5c60fa76']#
 iterations = 4
 parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument("--skip_training", action="store_true")
@@ -60,15 +60,15 @@ for n_classes, lambda_sem in [[0,0.0],[64,0.001]]:
             result_file = os.path.join(args.output_path, f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i}", "results.json")
             with open(result_file, "r") as f:
                 data = json.load(f)
-                ssim = data["SSIM"]
-                lpips = data["LPIPS"]
-                psnr = data["PSNR"]
+                ssim = data["ours_30000"]["SSIM"]
+                lpips = data["ours_30000"]["LPIPS"]
+                psnr = data["ours_30000"]["PSNR"]
                 results.append((ssim, lpips, psnr))
 
     # Compute averages
-    avg_ssim = sum([result[0] for result in results]) / len(results)/iterations
-    avg_lpips = sum([result[1] for result in results]) / len(results)/iterations
-    avg_psnr = sum([result[2] for result in results]) / len(results)/iterations
+    avg_ssim = sum([result[0] for result in results]) / len(results)
+    avg_lpips = sum([result[1] for result in results]) / len(results)
+    avg_psnr = sum([result[2] for result in results]) / len(results)
     print(f"DS with {n_classes} ")
     print("Average SSIM:", avg_ssim)
     print("Average LPIPS:", avg_lpips)
