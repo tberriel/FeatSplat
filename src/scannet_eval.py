d5c6eccd85e5fdbc8ac299ba84e191131ce834f8
tberriel <57666961+tberriel@users.noreply.github.com>
2024-04-03 12:01:12 +0200

    Added sh_degree as parameter to trainning scripts

--- src/scannet_eval.py
+++ src/scannet_eval.py
@@ -13,13 +13,14 @@ import os
 from argparse import ArgumentParser
 from metrics import evaluate
 import json
-scannetpp_scenes = ['a5114ca13d','108ec0b806','bb87c292ad', 'ebc200e928', 'a08d9a2476','5942004064',]#  ['0a5c013435', 'f07340dfea',  '7bc286c1b6', 'd2f44bf242',  '85251de7d1', '0e75f3c4d9', '98fe276aa8', '7e7cd69a59', 'f3685d06a9', '21d970d8de', '8b5caf3398', 'ada5304e41', '4c5c60fa76']#
-iterations = 4
+scannetpp_scenes = ['0a5c013435', 'f07340dfea',  '7bc286c1b6', 'd2f44bf242',  '85251de7d1', '0e75f3c4d9', '98fe276aa8', '7e7cd69a59', 'f3685d06a9', '21d970d8de', '8b5caf3398', 'ada5304e41', '4c5c60fa76']#['a5114ca13d','108ec0b806','bb87c292ad', 'ebc200e928', 'a08d9a2476','5942004064',]#
 parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument("--skip_training", action="store_true")
 parser.add_argument("--skip_rendering", action="store_true")
 parser.add_argument("--skip_metrics", action="store_true")
 parser.add_argument("--output_path", default="./mymodels/dgx1/id_0005")
+parser.add_argument("--sh_degree", default=0, type=int)
+parser.add_argument("--iterations", default=4, type=int)
 args, _ = parser.parse_known_args()
 
 all_scenes = []
@@ -35,8 +36,8 @@ if not args.skip_rendering:
         all_sources.append(args.scannetpp + "/" + scene)
  
 
-    common_args = " --quiet --eval --skip_train"
-    for i in range (iterations):
+    common_args = f" --quiet --eval --skip_train --sh_degree {args.sh_degree}"
+    for i in range (args.iterations):
         for n_classes, lambda_sem in [[0,0.0],[64,0.001]]:
             for scene, source in zip(all_scenes, all_sources):
                 #os.system("python src/render.py --iteration 7000 -s " + source + " -m " + args.output_path + "/" + f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i}" + common_args)
@@ -45,7 +46,7 @@ if not args.skip_rendering:
 if not args.skip_metrics:
     scenes_list = []
     scenes_string = ""
-    for i in range(iterations):
+    for i in range(args.iterations):
         for n_classes, lambda_sem in [[0,0.0],[64,0.001]]:
             for scene in all_scenes:
                 scenes_list.append(args.output_path + "/" + f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i}")
@@ -54,7 +55,7 @@ if not args.skip_metrics:
 
 for n_classes, lambda_sem in [[0,0.0],[64,0.001]]:
     results = []
-    for i in range(iterations):
+    for i in range(args.iterations):
         for scene in all_scenes:
 
             result_file = os.path.join(args.output_path, f"scannet_sem{n_classes}_1h_{lambda_sem}_{scene}_{i}", "results.json")
