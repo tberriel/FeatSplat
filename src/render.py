2b2c9f7ffce98a12904347a78986788ba38ac238
tberriel <57666961+tberriel@users.noreply.github.com>
2024-04-07 16:25:23 +0200

    Fixed wrong argument order

--- src/render.py
+++ src/render.py
@@ -75,4 +75,4 @@ if __name__ == "__main__":
     # Initialize system state (RNG)
     safe_state(args.quiet)
 
-    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args.save, args.gaussian_splatting)
+    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args.gaussian_splatting, args.save)
