f61a2128f990aaa7e6b53e373f95d286797d7674
tberriel <57666961+tberriel@users.noreply.github.com>
2024-05-17 18:02:46 +0200

    Fixed arg bug

--- src/full_eval.py
+++ src/full_eval.py
@@ -28,7 +28,7 @@ parser.add_argument("--skip_comp_metrics", action="store_true")
 parser.add_argument("--test_midway", action="store_true")
 parser.add_argument("--output_path", default="./eval")
 parser.add_argument("--n_classes", default=0, type=int)
-parser.add_argument("--iterations", default=30_000, type=int)
+parser.add_argument("--train_iterations", default=30_000, type=int)
 parser.add_argument("--h_layers", default=0, type=int)
 parser.add_argument("--n_neurons", default=64, type=int)
 parser.add_argument("--lambda_sem", default=0.001, type=float)
@@ -72,7 +72,7 @@ if len(args.scannetpp_scenes) == 0 and args.scannetpp:
         assert False, "Either pass a set of ScanNet++ scenes, or select a set from ['scannet_sem', 'scannet_nvs', 'scannet_rest']"
 
 if not args.skip_training:
-    common_args = f" --quiet --eval --iterations {args.iterations} --n_classes {args.n_classes} --sh_degree {args.sh_degree} --data_device {args.data_device} --h_layers {args.h_layers} --n_neurons {args.n_neurons}"
+    common_args = f" --quiet --eval --iterations {args.train_iterations} --n_classes {args.n_classes} --sh_degree {args.sh_degree} --data_device {args.data_device} --h_layers {args.h_layers} --n_neurons {args.n_neurons}"
     
     if not args.test_midway:
         common_args += " --test_iterations -1 "
@@ -145,6 +145,7 @@ if not args.skip_rendering:
             os.system("python src/render.py --iteration 21000 -s " + args.scannetpp + "/" + scene + " -m " + os.path.join(args.output_path,args.scannetpp_set,scene+name_suffix+f"_{i}") + " -r 1 "+ common_args)
             os.system("python src/render.py --iteration 30000 -s " + args.scannetpp + "/" + scene + " -m " + os.path.join(args.output_path,args.scannetpp_set,scene+name_suffix+f"_{i}") + " -r 1 "+ common_args)
             os.system("python src/render.py --iteration 35000 -s " + args.scannetpp + "/" + scene + " -m " + os.path.join(args.output_path,args.scannetpp_set,scene+name_suffix+f"_{i}") + " -r 1 "+ common_args)
+            os.system("python src/render.py --iteration 42000 -s " + args.scannetpp + "/" + scene + " -m " + os.path.join(args.output_path,args.scannetpp_set,scene+name_suffix+f"_{i}") + " -r 1 "+ common_args)
             os.system("python src/render.py --iteration 44000 -s " + args.scannetpp + "/" + scene + " -m " + os.path.join(args.output_path,args.scannetpp_set,scene+name_suffix+f"_{i}") + " -r 1 "+ common_args)
 
 
