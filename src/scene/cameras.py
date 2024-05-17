cdc9ad4d09f1fc1b5e827dec080ab89f82090672
tberriel <57666961+tberriel@users.noreply.github.com>
2024-05-17 16:12:04 +0200

    Modified to store images as int and convert to float only to compute loss

--- src/scene/cameras.py
+++ src/scene/cameras.py
@@ -36,14 +36,12 @@ class Camera(nn.Module):
             self.data_device = torch.device("cuda")
 
         self.original_semantic = semantic_image#.to(self.data_device) if semantic_image is not None else None
-        self.original_image = image.clamp(0.0, 1.0).to(self.data_device)
+        self.original_image = image.to(self.data_device)
         self.image_width = self.original_image.shape[2]
         self.image_height = self.original_image.shape[1]
 
         if gt_alpha_mask is not None:
             self.original_image *= gt_alpha_mask.to(self.data_device)
-        else:
-            self.original_image *= torch.ones((1, self.image_height, self.image_width), device=self.data_device)
 
         self.zfar = 100.0
         self.znear = 0.01
--- src/train.py
+++ src/train.py
@@ -139,7 +139,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         else: 
             Lce = 0.
         
-        gt_image = viewpoint_cam.original_image.cuda()
+        gt_image = viewpoint_cam.original_image.cuda() / 255.0
         Ll1 = l1_loss(image, gt_image)
         loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image)) + Lce*opt.lambda_sem
         loss.backward()
